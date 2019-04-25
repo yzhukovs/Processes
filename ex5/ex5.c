8efe52744f4949ddd446a3c4ce34ede8a4ef6a53
yzhukovs <yzhukovs@bumnetworks.com>
2019-04-25 18:46:44 -0400

    ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -7,16 +7,38 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 
 #define MSGSIZE 16
+
 
 char* msg1 = "hello world #1";
 char* msg2 = "hello world #2";
 char* msg3 = "hello world #3";
 
+int fd1[2];
+
 int main(void)
 {
-    // Your code here
+    char inbuf[MSGSIZE];
+    int p[2], i;
     
+    if (pipe(p) < 0)
+        exit(1);
+    
+    /* continued */
+    /* write pipe */
+    
+    write(p[1], msg1, MSGSIZE);
+    write(p[1], msg2, MSGSIZE);
+    write(p[1], msg3, MSGSIZE);
+    
+    for (i = 0; i < 3; i++) {
+        /* read pipe */
+        read(p[0], inbuf, MSGSIZE);
+        printf("%s\n", inbuf);
+    }
     return 0;
 }
+    
+
